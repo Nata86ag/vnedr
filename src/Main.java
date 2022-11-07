6f1d0215454ae98fd83e189bb974bea741fff499
Natalya Kolotygina <natahud86ag@mail.ru>
2022-11-07 22:45:42 +0500

    Add average

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
+import com.sun.source.util.SourcePositions;
+
 public class Main {
     public static void main(String[] args) {
-        SalesManager salesManager = new SalesManager(new int[] {1,3,8});
+        SalesManager salesManager = new SalesManager(new int[]{1, 3, 8, 100, 0});
         System.out.println(salesManager.max());
+        System.out.println(salesManager.getAverageSale());
     }
 }
