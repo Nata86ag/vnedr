4180f49dd049dec2b40c172fce6737d9c2ba3745
Natalya Kolotygina <natahud86ag@mail.ru>
2022-11-07 22:10:55 +0500

    longer

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        SalesManager salesManager = new SalesManager(new int[] {1,3,8});
+        SalesManager salesManager = new SalesManager(new long[] {1,3,8});
         System.out.println(salesManager.max());
     }
 }
